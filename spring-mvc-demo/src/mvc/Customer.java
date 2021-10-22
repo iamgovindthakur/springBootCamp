09c70ec0c685016a025150696fcb1b792d33a977
iamgovindthakur <iamgovindthakur@gmail.com>
2021-10-22 16:44:38 +0530

    added regexp hibernate validation for postal code field

--- spring-mvc-demo/src/mvc/Customer.java
+++ spring-mvc-demo/src/mvc/Customer.java
@@ -3,6 +3,7 @@ package mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -17,6 +18,18 @@ public class Customer {
 	@Min(value = 0, message = "must be greater than or equal to zero")
 	@Max(value = 10, message = "must be less than or equal to 10")
 	private int freePasses;
+	
+	@Pattern(regexp="^[0-9]{6}", message="only 5 digits")
+	private String postalCode;
+
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
 
 	public int getFreePasses() {
 		return freePasses;
